bf76b142c3c58e5e6fc38e1c9c21dfaf5cc4f3b6
MikeAirone <36142798+MikeAirone@users.noreply.github.com>
2019-02-25 22:06:19 +0700

    Update lib.h

--- Lab2/src/lib.h
+++ Lab2/src/lib.h
@@ -4,8 +4,8 @@
 int sc_memoryInit();
 int sc_memorySet(int address, int value);
 int sc_memoryGet(int address, int* value);
-int sc_memorySave(char * filename);
-int sc_memoryLoad(char * filename);
+int sc_memorySave(FILE* filename);
+int sc_memoryLoad(FILE* filename);
 int sc_regInit();
 int sc_regSet(int registr, int value);
 int sc_regGet(int registr, int * value);
